abc2b0b6c6643e9c270bbca54dc192d1924132a2
saim2001 <saimrao49@gmail.com>
2023-01-15 01:20:14 +0500

    cats

--- e_bazar_fyp/e_bazar_app/urls.py
+++ e_bazar_fyp/e_bazar_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import vendorRegister
+from .views import vendorRegister,Product,Category
 
 vendor= vendorRegister()
+product=Product()
+categories=Category()
 urlpatterns = [
     path('', vendor.logIn, name="logIn"),
-    path('register',vendor.register,name="register")
+    path('register',vendor.register,name="register"),
+    path('addproduct',product.renselectCat,name="addproduct"),
+    path('selectcategory',product.selectCat,name="selectcategory"),
+    path('<str:parent>',product.selectSubCat,name="selectsubcategory"),
+    path('<str:subparent>',product.selectLeafCat,name="selectleafcategory")
+
 ]
