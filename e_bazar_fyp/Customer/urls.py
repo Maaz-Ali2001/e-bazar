8dd089e70f0da4329d8e1ff16c284b6be0d3bebb
maaz ali <maazkid2001@gmail.com>
2023-01-23 04:04:59 +0500

    Revert "cart done"
    This reverts commit dc9a9c5fe735a13376bbe809b17752bfbb1c0a89.

--- e_bazar_fyp/Customer/urls.py
+++ e_bazar_fyp/Customer/urls.py
@@ -4,7 +4,5 @@ customer=Customer()
 urlpatterns = [
     path('', customer.renHomePage, name="logIn"),
     path('productdetail/',customer.productdetail,name='productdetail'),
-    path('cart/',customer.add_to_cart,name='cart'),
-    path('order/',customer.order,name='order'),
-    path('register/',customer.register,name='register')
+    path('cart/',customer.add_to_cart,name='cart')
 ]
