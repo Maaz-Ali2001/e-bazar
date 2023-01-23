c587825691ee0c57e9d3fc890416b12bd49f34e9
saim2001 <saimrao49@gmail.com>
2023-01-23 06:03:55 +0500

    Prodcut details page

--- e_bazar_fyp/Customer/urls.py
+++ e_bazar_fyp/Customer/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import Customer
 customer=Customer()
 urlpatterns = [
-    path('', customer.renHomePage, name="logIn"),
+
+    path('home/', customer.renHomePage, name="logIn"),
+    path('detail/<str:product_id>/', customer.productdetail, name='productdetails'),
+    path('details/<str:id>/', customer.productdetail, name='productvardetail'),
     path('cart/',customer.add_to_cart,name='cart'),
-    path('<str:product_id>/',customer.productdetail,name='productdetail'),
-    path('<str:id>/',customer.productdetail,name='productvardetail'),
     path('order/',customer.order,name='order'),
     path('register/',customer.register,name='register'),
 
